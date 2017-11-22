6185baf185c8354531453b307ca8806a27222d27
Sascha Böck <wi16b142@technikum-wien.at>
2017-11-22 05:15:39 +0100

    added logging

---
+++ CD5_FileHandler/FileHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CD5_DataHandling
+{
+
+    public class FileHandler
+    {
+        private const string folder = @"../../../logfiles/";
+        private const string extension = ".txt";
+
+
+        public String[] Read(string name)
+        {
+
+            String[] lines = File.ReadAllLines(folder + name);
+            return lines;
+        }
+
+        public void Write(List<string> data)
+        {
+            File.WriteAllLines(folder + DateTime.Now.ToString("d") + "_" + DateTime.Now.ToFileTimeUtc() + extension, data.ToArray());
+        }
+
+        public string[] SearchLogFiles()
+        {
+            if (!Directory.Exists(folder)) { Directory.CreateDirectory(folder); }
+            DirectoryInfo info = new DirectoryInfo(folder);
+            var content = info.GetFiles("*" + extension);
+            string[] logfiles = new string[content.Length];
+            int i = 0;
+            foreach (var logfile in content)
+            {
+                logfiles[i] = logfile.Name;
+                i++;
+            }
+            return logfiles;
+        }
+
+        public void Delete(string name)
+        {
+            if (File.Exists(folder + name))
+            {
+                File.Delete(folder + name);
+            }
+        }
+
+        public bool CheckFileExists(string name)
+        {
+            return File.Exists(folder + name + extension);
+        }
+    }
+}
--- CD5_Server/ViewModel/MainViewModel.cs
+++ CD5_Server/ViewModel/MainViewModel.cs
@@ -1,9 +1,10 @@
+using CD5_DataHandling;
 using CD5_Server.Model;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System.Collections.ObjectModel;
-using System.Windows;
-using System;
+
+using System.Linq;
 
 namespace CD5_Server.ViewModel
 {
@@ -14,20 +15,33 @@ namespace CD5_Server.ViewModel
         private const int port = 10100;
         private const string ip = "127.0.0.1";
         public bool isConnected = false;
+        private FileHandler fileHandler;
         public string SelectedClient { get; set; }
+        public string SelectedLogfile { get; set; }
         public ObservableCollection<string> Msgs { get; set; }
         public ObservableCollection<string> Clients { get; set; }
-        
+        public ObservableCollection<string> LogMsgs { get; set; }
         public RelayCommand StartBtnClickCommand { get; set; }
         public RelayCommand StopBtnClickCommand { get; set; }
         public RelayCommand DropClientBtnClickCommand { get; set; }
         public RelayCommand SaveBtnClickCommand { get; set; }
+        public RelayCommand DropLogBtnClickCommand { get; set; }
+        public RelayCommand ShowBtnClickCommand { get; set; }
+        public ObservableCollection<string> LogFiles
+        {
+            get
+            {
+                return new ObservableCollection<string>(fileHandler.SearchLogFiles());
+            }
+        }
 
         public MainViewModel()
         {
             Clients = new ObservableCollection<string>();
             Msgs = new ObservableCollection<string>();
-
+            fileHandler = new FileHandler();
+            LogMsgs = new ObservableCollection<string>();
+            
             StartBtnClickCommand = new RelayCommand(() => 
             {
                 server = new Server(ip, port, GUIBroadcast); //instantiate server
@@ -41,8 +55,25 @@ namespace CD5_Server.ViewModel
                 isConnected = false;
             }, () => { return isConnected; }); //can execute?
 
+            ShowBtnClickCommand = new RelayCommand(() =>
+            {
+                LogMsgs = new ObservableCollection<string>(fileHandler.Read(SelectedLogfile));
+                RaisePropertyChanged("LogMessages");
+            }, () => { return SelectedLogfile != null; });
+
+            DropLogBtnClickCommand = new RelayCommand(() =>
+            {
+                fileHandler.Delete(SelectedLogfile);
+                RaisePropertyChanged("LogFiles");
+            }, () => { return SelectedLogfile != null; });
+
+            SaveBtnClickCommand = new RelayCommand(() =>
+            {
+                fileHandler.Write(Msgs.ToList());
+                RaisePropertyChanged("LogFiles");
+            }, () => { return Msgs.Count >= 1; } );
+
             DropClientBtnClickCommand = new RelayCommand(DropClientBtnClickMethod);
-            SaveBtnClickCommand = new RelayCommand(SaveBtnClickMethod);
 
         }
 
@@ -66,11 +97,6 @@ namespace CD5_Server.ViewModel
         {
             server.KickClient(SelectedClient); //kick from server
             Clients.Remove(SelectedClient); //remove from GUI
-        }
-
-        public void SaveBtnClickMethod()
-        {
-            //bonus
         }
     }
 }
